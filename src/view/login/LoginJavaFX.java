9494a6cd469179b2b117353d3d2a5d6456806f8f
2dam <you@example.com>
2022-10-19 10:00:17 +0200

    window packages implemented

---
+++ src/view/login/LoginJavaFX.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package view.login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2dam
+ */
+public class LoginJavaFX extends Application {
+    // put in there the file with the view.
+    private final String XMLFILE = "LoginFXMLDocument.fxml";
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(XMLFILE));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
