766f327b6219b0943ce3072f036ce1d02f58fc0a
2dam <you@example.com>
2022-10-21 14:22:34 +0200

    adding demo users on the dao to check the windows

--- src/logic/model/IUMImplementation.java
+++ src/logic/model/IUMImplementation.java
@@ -1,13 +1,20 @@
 package logic.model;
 
+import java.sql.Timestamp;
+
 import logic.objects.User;
 
 public class IUMImplementation implements IUserManager {
 
+    /*
+     * (non-Javadoc)
+     */
     @Override
     public User signIn(User pUser) {
-        // TODO Auto-generated method stub
-        return null;
+        Timestamp t = Timestamp.valueOf("");
+        return new User(0, "FBe9", "nereaoceja2003@gmail.com",
+         "Nerea Oceja", "abcd*1234", 
+        t, 1, 1);
     }
 
     @Override
