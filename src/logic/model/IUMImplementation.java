209c6b27550a9b1bb8fc37eb62d85909051f8394
2dam <you@example.com>
2022-10-26 12:53:05 +0200

    constructor error fixed

--- src/logic/model/IUMImplementation.java
+++ src/logic/model/IUMImplementation.java
@@ -3,16 +3,17 @@ package logic.model;
 import java.sql.Timestamp;
 
 import logic.objects.User;
+import logic.objects.UserPrivilege;
+import logic.objects.UserStatus;
 
 public class IUMImplementation implements IUserManager {
 
     @Override
     public User signIn(User pUser) {
-        User us = null;
-        pUser.setStatus(UserStatus.ENABLED);
-        pUser.setPrivilege(UserPrivilege.ADMIN);
-        us = new User(0, "FBe9", "nereaoceja2003@gmail.com", "Nerea Oceja", "abcd*1234", null, pUser.getStatus(), pUser.getPrivilege());
-        return us;
+        return 
+            new User(0, "FBe9", "nereaoceja2003@gmail.com", "Nerea Oceja",
+            "abcd*1234", null, UserStatus.ENABLED, UserPrivilege.ADMIN, null);
+
     }
 
     @Override
